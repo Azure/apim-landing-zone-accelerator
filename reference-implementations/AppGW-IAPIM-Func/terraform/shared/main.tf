e32cbdf4daffc389b70bc2f90576480cf2ac512d
Paromita Roy <paromita.roy@outlook.com>
2022-02-10 17:12:25 +0000

    rebasing

--- reference-implementations/AppGW-IAPIM-Func/terraform/shared/main.tf
+++ reference-implementations/AppGW-IAPIM-Func/terraform/shared/main.tf
@@ -1,5 +1,4 @@
 locals {
-<<<<<<< HEAD
   resource_suffix = "${var.workload_name}-${var.environment}-${var.location}-001"
 }
 
@@ -33,15 +32,4 @@ resource "azurerm_application_insights" "shared_apim_insight" {
   resource_group_name = azurerm_resource_group.shared_rg.name
   application_type    = "web"
   workspace_id        = azurerm_log_analytics_workspace.log_analytics_workspace.id
-=======
-    key_vault_name = substr("kv-${var.workload_name}-${var.deployment_environment}-${var.resource_group_location}-${var.resource_suffix}", 0, 24)
-}
-
-resource "azurerm_key_vault" "example" {
-  name                        = local.key_vault_name
-  location                    = var.resource_group_location
-  resource_group_name         = var.resource_group_name
-  tenant_id                   = var.tenant_id
-  sku_name                    = "standard"
->>>>>>> bd02ef0664f50b136832909aca0bedca4e7213c0
 }
