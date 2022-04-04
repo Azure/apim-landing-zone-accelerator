a94c0f20813845ac2f96b2f300d2b60fa04125f7
Pete Messina <petermessina@microsoft.com>
2022-04-04 08:39:39 -0400

    Updating cert config

--- reference-implementations/AppGW-IAPIM-Func/terraform/gateway/main.tf
+++ reference-implementations/AppGW-IAPIM-Func/terraform/gateway/main.tf
@@ -95,16 +95,10 @@ resource "azurerm_key_vault_certificate" "local_domain_certs" {
 
     x509_certificate_properties {
       extended_key_usage = ["1.3.6.1.5.5.7.3.1"]
-
       key_usage = [
-        "cRLSign",
-        "dataEncipherment",
         "digitalSignature",
-        "keyAgreement",
-        "keyCertSign",
-        "keyEncipherment",
+        "keyEncipherment"
       ]
-
       subject            = "CN=${var.fqdn}"
       validity_in_months = 12
     }
