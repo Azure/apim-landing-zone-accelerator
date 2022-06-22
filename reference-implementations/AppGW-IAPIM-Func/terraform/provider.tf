20ac32bde5e47dbf4827fd08aa682a656604b14b
Paromita Roy <55242088+paromitaroy@users.noreply.github.com>
2022-06-22 11:51:10 +0100

    Update provider.tf
    Adding the remote tf backend

--- reference-implementations/AppGW-IAPIM-Func/terraform/provider.tf
+++ reference-implementations/AppGW-IAPIM-Func/terraform/provider.tf
@@ -1,4 +1,12 @@
 terraform {
+  
+  backend "azurerm" {
+    storage_account_name = "apimlztfbackend "
+    container_name       = "terraform-state"
+    key                  = "terraform.tfstate"
+    
+  }
+  
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -7,6 +15,12 @@ terraform {
   }
 }
 
+# Configure the Microosft Azure provider 
 provider "azurerm" {
   features {}
+  
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
