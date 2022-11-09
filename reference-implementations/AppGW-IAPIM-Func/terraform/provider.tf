a5db3fa8ab7ed2cf6879eebb5cd5906d82b1b8a1
Jin Lee <jinle@microsoft.com>
2022-11-09 15:29:33 -0600

    wflow debug

--- reference-implementations/AppGW-IAPIM-Func/terraform/provider.tf
+++ reference-implementations/AppGW-IAPIM-Func/terraform/provider.tf
@@ -1,16 +1,21 @@
 terraform {
 
   backend "azurerm" {
-    resource_group_name  = "tfstate"
-    storage_account_name = "tfstate1259034575"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
+    resource_group_name = "rg-terraform"
+    storage_account_name = "apimlztfbackend"
+    container_name       = "terraform-state"
+    key                  = "es-apim-lza.tfstate"
+    
+    # resource_group_name  = "tfstate"
+    # storage_account_name = "tfstate1259034575"
+    # container_name       = "tfstate"
+    # key                  = "terraform.tfstate"
   }
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=2.95.0"
+      version = "~> 3.1"
     }
   }
 }
