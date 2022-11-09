7a64c690124d29e1c6fcffcbfa7f373ca8b9b54c
Jin Lee <jinle@microsoft.com>
2022-11-09 12:02:20 -0600

    testing with v3.1 azurerm for OIDC compatibility

---
+++ reference-implementations/AppGW-IAPIM-Func/terraform/provider.tf
@@ -0,0 +1,30 @@
+terraform {
+
+  backend "azurerm" {
+    resource_group_name = "rg-terraform"
+    storage_account_name = "apimlztfbackend"
+    container_name       = "terraform-state"
+    key                  = "es-apim-lza.tfstate"
+    # resource_group_name = "tfstate"
+    # storage_account_name = "tfstate1259034575"
+    # container_name       = "tfstate"
+    # key                  = "terraform.tfstate"
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.1"
+    }
+  }
+}
+
+# Configure the Microsft Azure provider
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
