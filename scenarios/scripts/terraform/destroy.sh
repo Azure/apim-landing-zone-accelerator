24c3a578e58de39ccf33cd598ed03078dffb9687
Luis <6314335+luisfeliz79@users.noreply.github.com>
2025-11-13 10:19:16 -0500

    fixed CRLF on sample.env, updated destroy.sh

--- scenarios/scripts/terraform/destroy.sh
+++ scenarios/scripts/terraform/destroy.sh
@@ -1,11 +1,13 @@
 # validate if wants to proceed
 
+source "./.env"
+echo "Using TFVARS: ../../apim-baseline/terraform/${ENVIRONMENT_TAG}.tfvars"
+
 echo "Do you want to destroy the deployment? (y/n)"
 read -r response
 
 if [[ $response =~ ^[Yy]$ ]]; then
-    source "./.env"
-    echo "Using TFVARS: ${ENVIRONMENT_TAG}.tfvars"
+
     cd ../../apim-baseline/terraform 
     terraform destroy --auto-approve -var-file="${ENVIRONMENT_TAG}.tfvars"
 else
